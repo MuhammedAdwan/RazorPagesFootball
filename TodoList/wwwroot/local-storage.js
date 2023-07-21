e6ef80dfc3ef06c11e67086cedde68d2ba7987f7
MuhammedAdwan <133061822+MuhammedAdwan@users.noreply.github.com>
2023-07-21 15:12:39 -0400

    removed the LocalStorage

--- TodoList/wwwroot/local-storage.js
+++
@@ -1,8 +0,0 @@
-window.localStorageInterop = {
-    setItem: function (key, value) {
-        localStorage.setItem(key, value);
-    },
-    getItem: function (key) {
-        return localStorage.getItem(key);
-    },
-};
